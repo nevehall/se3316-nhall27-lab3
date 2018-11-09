014940f5e57b35e8dfe8ba600d1db18ee1e09472
Neve Hall <nhall27@uwo.ca>
2018-11-09 01:09:08 +0000

    finishing the tutorial

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const bodyParser = require('body-parser');
 const product = require('./routes/product.route');
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://nhall27:Database3316@ds217360.mlab.com:17360/se3316-nhall27-lab3';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;   //or const????
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
 
 let port = process.env.PORT;
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,3 +1,5 @@
+//routes/products/route.js
+
 const express = require('express');
 const router = express.Router();
 
@@ -5,6 +7,10 @@ const router = express.Router();
 const product_controller = require('../controllers/product.controller');
 
 // a simple test url to check that all of our files are communicating correctly
-router.get('/test', product_controller.test);
+//router.get('/test', product_controller.test);
+router.post('/create', product_controller.product_create);
+router.get('/:id', product_controller.product_details);
+router.put('/:id/update', product_controller.product_update);
+router.delete('/:id/delete', product_controller.product_delete);
 
 module.exports = router;
